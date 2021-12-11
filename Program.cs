58f20a1343e1ca6fa4b78e6fa6ca32a13bbe7e35
ArtSamuylenko <89385274775@mail.ru>
2021-12-12 02:03:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace del120_140
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 120; i < 141; i++)
+            {
+                Console.Write($"\n{i} => ");
+                for (int j = 1; j < i + 1; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        Console.Write($"{j} ");
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
